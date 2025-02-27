0568f6dea71a3acea6b17525652433af31d8d25b
sujanshetty11 <sujanshetty66@gmail.com>
2025-02-27 13:30:43 +0530

    firt commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+# filepath: /C:/Users/sujan/todo_app/config/routes.rb
+Rails.application.routes.draw do
+  resources :tasks
+  # other routes...
+end
---
+++ seeds.rb
@@ -0,0 +1,2 @@
+Task.create(title: "Buy groceries", completed: false)
+Task.create(title: "Read a book", completed: true)
